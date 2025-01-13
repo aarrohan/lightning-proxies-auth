cb5f8a1c2c42491ef54a62cf471cbf3c98eb3c11
Abdullah Al Reza Rohan <developer.aarrohan@gmail.com>
2025-01-14 00:51:48 +0600

    feat(backend): add verify email backend

---
+++ src/app/verify-email/[verifyToken]/Content.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useEffect } from "react";
+import { backendServerBaseURL } from "@/utils/auth";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export default function Content({ verifyToken }: { verifyToken: string }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (verifyToken) {
+      async function handleVerifyEmail() {
+        await axios
+          .post(`${backendServerBaseURL}/auth/verify-email`, {
+            verifyToken: verifyToken,
+          })
+          .then((response) => {
+            if (response.status === 200) {
+              router.push("/email-verified");
+            }
+          })
+          .catch((error) => {
+            if (error.response.status === 400) {
+              router.push("/");
+            }
+          });
+      }
+
+      handleVerifyEmail();
+    }
+  }, []);
+
+  return;
+}
---
+++ src/app/verify-email/[verifyToken]/page.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+import Content from "./Content";
+
+export const metadata: Metadata = {
+  title: "Verify Email",
+  description:
+    "Verify your email address to gain access to your account. Please check your email for the verification link.",
+  openGraph: {
+    type: "website",
+    url: `${process.env.NEXT_PUBLIC_APP_URL}/verify-email/[verifyToken]`,
+    title: "Verify Email",
+    description:
+      "Verify your email address to gain access to your account. Please check your email for the verification link.",
+    images: `${process.env.NEXT_PUBLIC_WEB_URL}/images/og-img.png`,
+  },
+};
+
+export default function TwoFA({ params }: { params: { verifyToken: string } }) {
+  if (!params.verifyToken) return redirect("/");
+
+  return <Content />;
+}
