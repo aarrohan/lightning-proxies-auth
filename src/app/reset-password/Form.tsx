d2dfcbd8461ad2b454b797df9a18678dedfb8500
Abdullah Al Reza Rohan <developer.aarrohan@gmail.com>
2025-01-15 01:27:09 +0600

    feat(ui): work on changes

--- src/app/reset-password/Form.tsx
+++ src/app/reset-password/Form.tsx
@@ -1,37 +1,48 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import axios from "axios";
-import { backendServerBaseURL } from "@/utils/auth";
+import { backendServerBaseURL, googleRecaptchKey } from "@/utils/auth";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Form() {
   const [email, setEmail] = useState<string>("");
+
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
 
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    try {
-      await axios
-        .post(`${backendServerBaseURL}/auth/forgot-password`, {
-          email,
-        })
-        .then((response) => {
-          if (response.status === 200) {
-            router.push(`/reset-password/success?email=${email}`);
-          }
-        })
-        .catch((error) => {
-          if (error.response.status === 400) {
-            toast.error(error.response.data.message);
-          }
-        });
-    } catch (error) {
-      if (error) {
-        toast.error(error as string);
+    const recaptchaValue = recaptchaRef.current?.getValue();
+
+    if (recaptchaValue) {
+      try {
+        recaptchaRef.current?.reset();
+
+        await axios
+          .post(`${backendServerBaseURL}/auth/forgot-password`, {
+            email,
+          })
+          .then((response) => {
+            if (response.status === 200) {
+              router.push(`/reset-password/success?email=${email}`);
+            }
+          })
+          .catch((error) => {
+            if (error.response.status === 400) {
+              toast.error(error.response.data.message);
+            }
+          });
+      } catch (error) {
+        if (error) {
+          toast.error(error as string);
+        }
       }
+    } else {
+      toast.error("Please verify that you are not a robot.");
     }
   };
 
@@ -55,6 +66,18 @@ export default function Form() {
           className="w-full py-2.5 px-3.5 focus:!ring-4 focus:!ring-accent/20 border border-[#D0D5DD] focus:border-accent rounded-lg text-sm sm:text-base tracking-[-0.14px] sm:tracking-[-0.16px] placeholder:text-primary/50 duration-200"
           style={{
             boxShadow: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)",
+          }}
+        />
+      </div>
+
+      <div className="mb-5 flex justify-center">
+        <ReCAPTCHA
+          sitekey={googleRecaptchKey}
+          ref={recaptchaRef}
+          onChange={(value) => {
+            if (value) {
+              sessionStorage.setItem("recaptchaToken", value);
+            }
           }}
         />
       </div>
